64d4f8e09607e4ac5ddbbe95bee0e78b857ca745
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-04-07 01:07:49 -0300

    ok

--- atualizacluster.sh
+++ atualizacluster.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-git add .
-git commit -m "tilt commit"
-git push
+echo "
+Você já efetuou o 
+
+######   ######   ##   ##   ##   ##  ######  ######    #####
+#        #    #   # # # #   # # # #    #       #           #
+#        #    #   #  #  #   #  #  #    #       #        ###
+#        #    #   #     #   #     #    #       #       
+#####    ######   #     #   #     #  #####     #        #
+
+A as modificações do cluster de demo serão somente refletidas após o commit nesta branch!
+"
+
 kubectl create configmap -n demo-materialescolar --dry-run material-escolar-backend --from-env-file=.env --output yaml | tee configmap.yaml
 kubectl get deployment --all-namespaces | awk 'match($6,/[0-9]+d/) {print $1}' | grep demo | kubectl scale deploy -n demo-materialescolar --replicas=1 --all
 sleep 5
